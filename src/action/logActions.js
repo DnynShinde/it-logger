5c19b4a9114aa4c326a8312c2366683d8794567a
Dnyn Shinde <shindekartik811@gmail.com>
2020-09-25 11:01:02 +0530

    Completed Setup for Redux

---
+++ src/action/logActions.js
@@ -0,0 +1,26 @@
+import { SET_LOADING, GET_LOGS, LOGS_ERROR } from "./types";
+
+// Get logs from server
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    return {
+      type: GET_LOGS,
+      payload: data,
+    };
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
---
+++ src/reducers/logReducer.js
@@ -0,0 +1,34 @@
+import { SET_LOADING, GET_LOGS, LOGS_ERROR } from "../action/types";
+
+const initialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false,
+      };
+
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LOGS_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return {
+        state,
+      };
+  }
+};
